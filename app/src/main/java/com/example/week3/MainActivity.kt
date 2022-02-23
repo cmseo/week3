9dbbd4bfbd56d0f3436dd4a1f03d4f77e8df7a32
changmu seo <changmu.seo@lge.com>
2022-02-23 12:44:20 +0900

    ing

--- app/src/main/java/com/example/week3/MainActivity.kt
+++ app/src/main/java/com/example/week3/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.week3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.week3.ui.theme.Week3Theme
 
 class MainActivity : ComponentActivity() {
@@ -26,9 +30,81 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
+/**
+ * Note: In this code snippet, we're using CompositionLocalProvider.
+ * It allows us to pass data implicitly through the composition tree.
+ * In this case, we're accessing ContentAlpha.medium, the medium opacity level, which is defined at the theme level in this case by MaterialTheme.
+ *
+*/
+/*@Composable
 fun PhotographerCard() {
+    Column() {
+        Text("Aflred Sisley", fontWeight = FontWeight.Bold)
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+        }
+    }
+}*/
+@Composable
+fun PhotographerCard1() {
+    Row(modifier = Modifier.padding(16.dp)
+        .clickable(onClick = {/* click event */})) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // Image
+        }
+        Column(modifier = Modifier
+            .padding(start = 8.dp)
+            .align(Alignment.CenterVertically)) {
+            Text("Aflred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
 
+@Composable
+fun PhotographerCard2() {
+    Row(modifier = Modifier
+        .clickable(onClick = {/* click event */}
+            .padding(16.dp)
+        )) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+            // Image
+        }
+        Column(modifier = Modifier
+            .padding(start = 8.dp)
+            .align(Alignment.CenterVertically)) {
+            Text("Aflred Sisley", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview1() {
+    Week3Theme {
+        PhotographerCard1()
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview2() {
+    Week3Theme {
+        PhotographerCard2()
+    }
 }
 
 @Composable
@@ -36,10 +112,11 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+/*
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Week3Theme {
         Greeting("Android")
     }
-}
+}*/
